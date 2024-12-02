df903c6b3ada5c538cea807cd35f2a438dfbffc4
KaSin TV <158577616+kasintv2-0@users.noreply.github.com>
2024-12-02 21:02:34 +0545

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $mpdUrl = 'https://dai.fancode.com' . $get;
 $mpdheads = [
   'http' => [
       'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0\r\n",
+   "Referer: https://www.fancode.com/\r\n", // Updated with the Fancode referrer
       'follow_location' => 1,
       'timeout' => 5
   ]
